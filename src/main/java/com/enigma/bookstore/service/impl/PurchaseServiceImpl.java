880602d1323ed26eb72a9fc7a525249f02eb107c
Inez Yulia Amanda <inez.amanda16@gmail.com>
2021-09-15 21:52:51 +0800

    reorganized code

--- src/main/java/com/enigma/bookstore/service/impl/PurchaseServiceImpl.java
+++ src/main/java/com/enigma/bookstore/service/impl/PurchaseServiceImpl.java
@@ -5,7 +5,6 @@ import com.enigma.bookstore.entity.Book;
 import com.enigma.bookstore.entity.Member;
 import com.enigma.bookstore.entity.Purchase;
 import com.enigma.bookstore.entity.PurchaseDetail;
-import com.enigma.bookstore.repository.PurchaseDetailRepository;
 import com.enigma.bookstore.repository.PurchaseRepository;
 import com.enigma.bookstore.service.BookService;
 import com.enigma.bookstore.service.MemberService;
@@ -15,9 +14,6 @@ import exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class PurchaseServiceImpl implements PurchaseService {
@@ -36,21 +32,26 @@ public class PurchaseServiceImpl implements PurchaseService {
     @Override
     @Transactional
     public Purchase transaction(Purchase purchase) {
-        Member member = memberService.getMemberById(purchase.getMember().getId());
         Purchase purchase1 = purchaseRepository.save(purchase);
+
+        Member member = memberService.getMemberById(purchase.getMember().getId());
         purchase1.setMember(member);
 
         for (PurchaseDetail purchaseDetail: purchase.getPurchaseDetails()) {
             purchaseDetail.setPurchase(purchase1);
             Book book = bookService.getBookById(purchaseDetail.getBook().getId());
+
             if (book.getStock() == 0)
                 throw new ResourceNotFoundException("Book is out of stock");
+
             if (book.getStock() < purchaseDetail.getQuantity()) {
                 String message = String.format(ResponseMessage.BAD_REQUEST, book.getStock(), purchaseDetail.getQuantity(), book.getTitle());
                 throw new ResourceNotFoundException(message);
             }
+
             book.setStock(book.getStock()- purchaseDetail.getQuantity());
             bookService.updateBook(book);
+
             purchaseDetail.setPriceSell((double) (book.getPrice() * purchaseDetail.getQuantity()));
             purchaseDetail.setBook(book);
             purchaseDetailService.savePurchaseDetail(purchaseDetail);
