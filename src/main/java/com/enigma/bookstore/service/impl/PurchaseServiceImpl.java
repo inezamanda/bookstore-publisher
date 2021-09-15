aa665afc620719f27024bd3e7bcb20cc0c0ef0cf
Inez Yulia Amanda <inez.amanda16@gmail.com>
2021-09-15 20:09:17 +0800

    return book and member information (password still accessible)

--- src/main/java/com/enigma/bookstore/service/impl/PurchaseServiceImpl.java
+++ src/main/java/com/enigma/bookstore/service/impl/PurchaseServiceImpl.java
@@ -2,11 +2,13 @@ package com.enigma.bookstore.service.impl;
 
 import com.enigma.bookstore.constant.ResponseMessage;
 import com.enigma.bookstore.entity.Book;
+import com.enigma.bookstore.entity.Member;
 import com.enigma.bookstore.entity.Purchase;
 import com.enigma.bookstore.entity.PurchaseDetail;
 import com.enigma.bookstore.repository.PurchaseDetailRepository;
 import com.enigma.bookstore.repository.PurchaseRepository;
 import com.enigma.bookstore.service.BookService;
+import com.enigma.bookstore.service.MemberService;
 import com.enigma.bookstore.service.PurchaseDetailService;
 import com.enigma.bookstore.service.PurchaseService;
 import exception.ResourceNotFoundException;
@@ -28,10 +30,15 @@ public class PurchaseServiceImpl implements PurchaseService {
     @Autowired
     BookService bookService;
 
+    @Autowired
+    MemberService memberService;
+
     @Override
     @Transactional
     public Purchase transaction(Purchase purchase) {
+        Member member = memberService.getMemberById(purchase.getMember().getId());
         Purchase purchase1 = purchaseRepository.save(purchase);
+        purchase1.setMember(member);
 
         for (PurchaseDetail purchaseDetail: purchase.getPurchaseDetails()) {
             purchaseDetail.setPurchase(purchase1);
@@ -45,6 +52,7 @@ public class PurchaseServiceImpl implements PurchaseService {
             book.setStock(book.getStock()- purchaseDetail.getQuantity());
             bookService.updateBook(book);
             purchaseDetail.setPriceSell((double) (book.getPrice() * purchaseDetail.getQuantity()));
+            purchaseDetail.setBook(book);
             purchaseDetailService.savePurchaseDetail(purchaseDetail);
         }
 
